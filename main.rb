5a757c275ec9421b1ed0a8eb300f58154e8521ce
Mr.Pain <ramil.talipov@gmail.com>
2017-04-10 00:00:30 +0300

    version 2

--- main.rb
+++ main.rb
@@ -50,11 +50,11 @@ for item in questions do
 end
 
 puts case answer_counter
-       when (30..32) then results[0]
-       when (25..29) then results[1]
-       when (19..24) then results[2]
-       when (14..18) then results[3]
-       when (9..13) then results[4]
-       when (4..8) then results[5]
-       else results[6]
+     when (30..32) then results[0]
+     when (25..29) then results[1]
+     when (19..24) then results[2]
+     when (14..18) then results[3]
+     when (9..13) then results[4]
+     when (4..8) then results[5]
+     else results[6]
      end
